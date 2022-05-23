86b6696e8de0827c2734751b9dbf4acd6827d251
Carolina Stahl <stahl@langeit.com>
2022-05-23 17:12:07 +0200

    finished Associations & Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cars
-  validates :username, :first_name, :last_name :date_of_birth, presence: true
+  validates :username, :first_name, :last_name, :date_of_birth, presence: true
   validates :username, :email, uniqueness: true
 
 end
