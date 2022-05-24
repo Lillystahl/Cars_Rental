61f5c58fb899a6765a2303486be54c71fcd49566
Carolina Stahl <stahl@langeit.com>
2022-05-24 15:11:12 +0200

    Added cards

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
-  # validates :title, :price, presence: true
-  # validates :description, length: { minimum: 20 }
-  # validates :availability, inclusion: [true, false]
+  validates :title, :price, presence: true
+  validates :description, length: { minimum: 20 }
+  validates :availability, inclusion: [true, false]
 end
