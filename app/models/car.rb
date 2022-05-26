3d77d79f164d923a6c92ab548b164c28c9bf7308
Carolina Stahl <stahl@langeit.com>
2022-05-26 15:58:51 +0200

    Form almost finished

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,6 @@ class Car < ApplicationRecord
   validates :title, :price, presence: true
   validates :description, length: { minimum: 8 }
   # validates :availability, inclusion: [true, false]
-
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 end
