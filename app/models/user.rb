0b237f6a17d1dbbc93ae54e2736feb8cafdd13ec
Carolina Stahl <stahl@langeit.com>
2022-05-26 15:13:00 +0200

    Booking feature works

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cars
+  has_many :bookings
   # validates :username, :first_name, :last_name, :date_of_birth, presence: true
   # validates :username, :email, uniqueness: true
 
@@ -13,9 +14,9 @@ class User < ApplicationRecord
   private
 
   def validate_age
-      if date_of_birth.present? && date_of_birth > 18.years.ago
-          errors.add(:date_of_birth, 'You should be over 18 years old.')
-      end
+    if date_of_birth.present? && date_of_birth > 18.years.ago
+      errors.add(:date_of_birth, 'You should be over 18 years old.')
+    end
   end
-  
+
 end
